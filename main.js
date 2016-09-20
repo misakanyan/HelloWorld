303bc7f16e7e11b0e72ffb3168d3e21dddc2aaed
lenovo\lenovo-pc <2213286021@qq.com>
2016-09-20 20:51:20 +0900

    作业３完成了，但还要看看是否要修正

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function display(x) {
     var str = "";
-    for (var j = 0; j < x; j++) {
+    for (var j = 1; j <= x; j++) {
         str = "";
-        for (var k = 0; k < x - j - 1; k++) {
-            str += "　";
-            for (var i = 0; i < 2 * j + 1; i++) {
-                str += "*";
-            }
+        for (var k = 1; k <= x - j; k++) {
+            str += " ";
+        }
+        for (var k = x - j + 1; k <= x + j - 1; k++) {
+            str += "*";
+        }
+        for (var k = x + j; k <= x * 2 + 1; k++) {
+            str += "";
         }
         console.log(str);
     }
--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 function display(x:number):void {
     var str : string = "";
 
-        for(var j=0;j<x;j++) {
+        for(var j=1;j<=x;j++) {
             str = "";
-            for(var k=0;k<x-j-1;k++) {
-                str+="　";
+            for(var k=1;k<=x-j;k++){
+                str+=" ";
             }
-            for(var i=0;i<2*j+1;i++) {
+            for(var k=x-j+1;k<=x+j-1;k++){
                 str+="*";
+            }
+            for(var k=x+j;k<=x*2+1;k++){
+                str+=" ";
             }
             console.log(str);
         }
