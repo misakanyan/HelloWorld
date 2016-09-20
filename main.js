1d186197b301ca5c35737a67e9326c4a38e78fd0
lenovo\lenovo-pc <2213286021@qq.com>
2016-09-20 15:31:54 +0900

    写了一个显示“miao”的程序 用于测试

---
+++ main.js
@@ -0,0 +1,5 @@
+function display() {
+    var str = "miao";
+    console.log(str);
+}
+display();
---
+++ main.ts
@@ -0,0 +1,6 @@
+function display():void {
+    var str : string = "miao"
+    console.log(str);
+}
+
+display();
